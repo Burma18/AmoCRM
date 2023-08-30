da2f11c4cd01ca310ee712bb0db71749b23fca91
Burma Rysmatova <rysmatovaburma@gmail.com>
2023-08-30 15:10:25 +0600

    updated with kommo credentials

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -5,11 +5,12 @@ import smscontrollers from "../modules/sendSms/sendSms";
 import leads from "../modules/leads/leads.routes";
 
 const routes = async (app: FastifyInstance) => {
-  app.register(leads, {
-    prefix: "/leads",
-  });
+  // app.register(leads, {
+  //   prefix: "/",
+  // });
 
   app.get("/:name", controller.getByQuery);
+  app.get("/getAll", controller.get);
   app.post("/api/v1/send-sms", smscontrollers.sendSmsCodeVerify);
   app.post("/api/v1/submissions", exercisesControllers.createSubmission);
 };
